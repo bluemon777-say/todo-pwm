2872dd101f2f1aa63a255f99fdb90f1d05f0ef1d
bluemon777-say <bluemon777@gmail.com>
2026-02-20 19:08:32 +0900

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("memoAPI", {
-  list: () => ipcRenderer.invoke("memos:list"),
-  add: (text) => ipcRenderer.invoke("memos:add", text),
-  delete: (id) => ipcRenderer.invoke("memos:delete", id)
-});
