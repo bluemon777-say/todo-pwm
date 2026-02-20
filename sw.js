86fbd78fc09a9d422c7309f44f5d0373298e24c0
bluemon777-say <bluemon777@gmail.com>
2026-02-20 19:44:23 +0900

    Update cache version in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "todo-pwa-cache-v1";
+const CACHE = "todo-pwa-cache-v1.5";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
 
 self.addEventListener("install", (event) => {
